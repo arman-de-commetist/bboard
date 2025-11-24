04d4ce004e2ca485594633694cdb7b96ee1a4fc9
Arman <armanospanov212@gmail.com>
2025-11-24 18:44:11 +0500

    lesson_03 files

---
+++ bboard/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.views.generic import CreateView
+
+from bboard.forms import BbForm
+from bboard.models import Bb, Rubric
+
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    success_url = '/'
+
+    def get_context_data(self, **kwargs):
+        context = super
+def index(request):
+    template = loader.get_template('index.html')
+    bbs = Bb.objects.all()
+    rubrics = Rubric.objects.all()
+    context = {'bbs': bbs, 'rubrics': rubrics}
+    return HttpResponse(template.render(context, request))
+
+
+def by_rubric(request, rubric_id):
+    bbs = Bb.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {
+        'bbs': bbs,
+        'rubrics': rubrics,
+        'current_rubric': current_rubric
+    }
+    return render(request, 'board.html', context)
